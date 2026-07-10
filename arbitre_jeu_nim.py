dfc1c5e716f93cf7c8a68b5c3dc5bf5d97c3da13
Arnaud Delperier <arnaud.delperier@fms-ea.com>
2026-07-10 15:52:44 +0200

    Ajout de la méthode permettant de mener une partie à deux joueurs (input et logique)

--- arbitre_jeu_nim.py
+++ arbitre_jeu_nim.py
@@ -7,15 +7,45 @@ def init_game():
         print("Veuillez répondre ""o"" ou ""n"".")
 
     is_versus_player = user_input in ("oui", "o")
-    launch_two_player_game() if is_versus_player else launch_ia_game()
+    two_player_game() if is_versus_player else ia_game()
 
 
-def launch_two_player_game():
+def two_player_game():
+    matches = 21
     player_one = ask_name("Choisissez le pseudo du premier joueur : ")
     player_two = ask_name("Choisissez le pseudo du second joueur : ")
 
-    starting_player = random.choice([player_one, player_two])
-    print(f"Tirage au sort du joueur qui jouera en premier...\n{starting_player} commence la partie !")
+    current_player = random.choice([player_one, player_two])
+    print(f"Tirage au sort du joueur qui jouera en premier...\n{current_player} commence la partie !")
+
+    while True:
+        if matches == 1:
+            print(f"il ne reste plus qu'une allumette à retirer...\nDésolé {current_player} vous perdez la partie !")
+            break
+
+        user_input = input(f"{current_player} combien d'allumettes voulez-vous retirer ? (1 à 4) : ")
+
+        if user_input.isdigit() and 1 <= int(user_input) <= 4:
+
+            if int(user_input) > matches >= 1:
+                print(f"il ne reste que {matches} allumettes. Vous ne pouvez pes en retirer {user_input} !")
+                continue
+            else:
+                matches -= int(user_input)
+                print(f"il reste {matches} allumettes")
+
+            if matches == 0:
+                print(f"Oups ! Vous avez retirez toutes les allumettes restantes, vous "
+                      f"perdez la partie {current_player}")
+                break
+
+        else:
+            print("Veuillez entrer un nombre entre 1 et 4.")
+            continue
+
+        current_player = player_two if current_player == player_one else player_one
+        print("\n###########################################\n")
+        print(f"Au tour de {current_player} de jouer.")
 
 
 def ask_name(prompt):
@@ -28,7 +58,7 @@ def ask_name(prompt):
         print("Le pseudo doit contenir 16 caractères au maximum.")
 
 
-def launch_ia_game():
+def ia_game():
     pass
 
 
